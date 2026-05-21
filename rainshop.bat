446ec664d070037c1f86ba4f711e4071a39a05e8
alaric2001 <alaric2001ra@gmail.com>
2026-05-21 11:14:17 +0700

    update

--- rainshop.bat
+++ rainshop.bat
@@ -1,17 +1,15 @@
 @echo off
 
+REM === Jalankan Backend (FastAPI) ===
+start cmd /k "cd /d C:\Users\Rain Shop\Documents\SistemRainShop\rainshop && uvicorn app.main:app --reload"
 
-@echo off
-REM === Jalankan Backend ===
-start cmd /k "cd /d C:\Users\Rain Shop\Documents\SistemRainShop\rainshop\rainshop_fastapi && uvicorn app.main:app --reload"
-
-REM === Jalankan Frontend ===
+REM === Jalankan Frontend (static webapp) ===
 start cmd /k "cd /d C:\Users\Rain Shop\Documents\SistemRainShop\rainshop\webapp && http-server -p 8080 --host 0.0.0.0"
 
 REM === Tunggu sebentar supaya server siap ===
 timeout /t 45 >nul
 
-REM === Buka Microsoft Edge di halaman Item List ===
+REM === Buka Microsoft Edge ===
 start "" "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" --new-tab http://127.0.0.1:8080/#/item-list
 
 pause
